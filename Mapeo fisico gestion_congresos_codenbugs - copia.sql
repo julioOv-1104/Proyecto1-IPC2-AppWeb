24e707179674cb6b267f63a488057dbb5069f2a4
julioOv-1104 <julioovalle202232237@cunoc.edu.gt>
2025-09-07 17:01:44 -0600

    inicio del repositorio

---
+++ Mapeo fisico gestion_congresos_codenbugs - copia.sql
@@ -0,0 +1,107 @@
+-- Cada clase ENUM con su nombre, las demas clases con sus metodos para registrar y sus atributos
+CREATE DATABASE IF NOT EXISTS gestion_congresos_codenbugs;
+
+USE gestion_congresos_codenbugs;
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id VARCHAR(20) NOT NULL,
+    institucion VARCHAR(100) NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    numero_telefonico VARCHAR(10) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    monedero double NOT NULL DEFAULT 0,
+    foto VARCHAR(100),
+    tipo_usuario ENUM('ADMIN_CONGRESO','ADMIN_SISTEMA','PATICIPANTE') NOT NULL,
+    CONSTRAINT pk_usuario PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS participante(
+nombre VARCHAR(100) NOT NULL,
+id_usuario VARCHAR(20) NOT NULL,
+tipo_participante ENUM('PONENTE','TALLERISTA','ASISTENTE') NOT NULL,
+CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE IF NOT EXISTS congreso(
+codigo_congreso VARCHAR(20) NOT NULL,
+fecha_inicio DATE NOT NULL,
+precio DOUBLE NOT NULL,
+CONSTRAINT pk_codigo_congreso PRIMARY KEY (codigo_congreso)
+);
+
+CREATE TABLE IF NOT EXISTS actividad(
+id_encargado VARCHAR(100) NOT NULL,
+descripcion VARCHAR(500) NOT NULL,
+hora_inicio TIME NOT NULL,
+hora_fin TIME NOT NULL,
+nombre_actividad VARCHAR(100) NOT NULL,
+codigo_actividad VARCHAR(20) NOT NULL,
+codigo_congreso VARCHAR(20) NOT NULL,
+tipo ENUM('PONENCIA','TALLER') NOT NULL,
+salon VARCHAR(100) NOT NULL,
+CONSTRAINT pk_codigo_actividad PRIMARY KEY (codigo_actividad),
+CONSTRAINT fk_codigo_congreso FOREIGN KEY (codigo_congreso) REFERENCES congreso(codigo_congreso),
+CONSTRAINT fk_id_encargado FOREIGN KEY (id_encargado) REFERENCES usuario(id)
+);
+
+CREATE TABLE IF NOT EXISTS inscripcion(
+codigo_congreso VARCHAR(20) NOT NULL,
+costo double NOT NULL DEFAULT 35,
+CONSTRAINT fk_codigo_congreso_inscripcion FOREIGN KEY (codigo_congreso) REFERENCES congreso(codigo_congreso)
+ );
+ 
+ Create TABLE IF NOT EXISTS pago(
+ codigo_congreso VARCHAR(20) NOT NULL,
+ fecha DATE NOT NULL,
+ monto double NOT NULL,
+ CONSTRAINT fk_codigo_congreso_pago FOREIGN KEY (codigo_congreso) REFERENCES congreso(codigo_congreso)
+  );
+ 
+ 
+ CREATE TABLE IF NOT EXISTS institucion(
+ codigo_congreso VARCHAR(20) NOT NULL,
+ id_administrador VARCHAR(100) NOT NULL,
+ nombre_institucion VARCHAR(100) NOT NULL,
+ CONSTRAINT pk_nombre_institucion PRIMARY KEY (nombre_institucion),
+ CONSTRAINT fk_codigo_congreso_institucion FOREIGN KEY (codigo_congreso) REFERENCES congreso(codigo_congreso),
+ CONSTRAINT fk_id_administrador FOREIGN KEY (id_administrador) REFERENCES usuario(id)
+ );
+ 
+ CREATE TABLE IF NOT EXISTS salones(
+ nombre_salon VARCHAR(100) NOT NULL,
+ institucion VARCHAR(100) NOT NULL,
+ CONSTRAINT pk_nombre_salon PRIMARY KEY (nombre_salon),
+ CONSTRAINT fk_institucion FOREIGN KEY (institucion) REFERENCES institucion(nombre_institucion)
+ );
+ 
+ CREATE TABLE IF NOT EXISTS instalacion(
+ nombre VARCHAR(100) NOT NULL,
+ codigo_congreso VARCHAR(20) NOT NULL,
+ CONSTRAINT pk_nombre PRIMARY KEY (nombre),
+ CONSTRAINT fk_codigo_congreso_instalacion FOREIGN KEY (codigo_congreso) REFERENCES congreso(codigo_congreso)
+ );
+ 
+ CREATE TABLE IF NOT EXISTS diploma(
+ codigo_congreso VARCHAR(20) NOT NULL,
+ id_participante VARCHAR(100) NOT NULL, 
+ CONSTRAINT fk_codigo_congreso_diploma FOREIGN KEY (codigo_congreso) REFERENCES congreso(codigo_congreso),
+ CONSTRAINT fk_id_participante FOREIGN KEY (id_participante) REFERENCES usuario(id)
+  );
+  
+  CREATE TABLE IF NOT EXISTS asistencia(
+  codigo_actividad VARCHAR(20) NOT NULL,
+  id_participante VARCHAR(20) NOT NULL,
+  CONSTRAINT fk_codigo_actividad FOREIGN KEY (codigo_actividad) REFERENCES actividad(codigo_actividad),
+  CONSTRAINT fk_id_participante_asistencia FOREIGN KEY (id_participante) REFERENCES usuario(id)
+  );
+  
+  ALTER TABLE usuario
+  ADD CONSTRAINT fk_institucion_usuario FOREIGN KEY (institucion) REFERENCES institucion(nombre_institucion);
+  
+  ALTER TABLE congreso
+  ADD COLUMN institucion VARCHAR(100) NOT NULL, 
+  ADD COLUMN instalacion VARCHAR(100)NOT NULL;
+  
+  ALTER TABLE congreso
+  ADD CONSTRAINT fk_institucion_congreso FOREIGN KEY (institucion) REFERENCES institucion(nombre_institucion),
+  ADD CONSTRAINT fk_instalacion FOREIGN KEY (instalacion) REFERENCES instalacion(nombre);
